817a17c115ab106a0ebcea129bea3c9aa4d9470b
ososabe <95254767+ososabe@users.noreply.github.com>
2022-10-19 11:06:52 -0300

    se agrega control de escaleno y pitagoras

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -16,7 +16,35 @@ public class App {
     if (a == b || a == c || b == c) {
       return "es un triangulo isoceles";
     }
+    
+    if (a != b && a != c && b != c) {
+      return "es un triangulo escaleno";
+    }
+    
+     if(esTrianguloPitagorico( a, b,  c)){
+      return "es un triangulo pitagorico";
+    }
+    
     return "error no es un triangulo";
+  }
+  
+  private static boolean esTrianguloPitagorico(int a,int b, int c){
+   //a2 + b2 = c2
+    if (a > b && a > c) {
+      if ((Math.pow(a,2)) == (Math.pow(b,2)) + (Math.pow(c,2))) {
+        return true;
+      } 
+    } else if (b > a  &&  b > c) {
+      if ((Math.pow(b,2)) == (Math.pow(a,2)) + (Math.pow(c,2))) {
+        return  true;
+      } 
+    } else if (c > a  &&  c > b) {
+      if ((Math.pow(c,2)) == (Math.pow(a,2)) + (Math.pow(b,2))) {
+        return  true;
+      } 
+	 } 
+    
+    return false;
   }
 
   public static void main(String[] args) {
